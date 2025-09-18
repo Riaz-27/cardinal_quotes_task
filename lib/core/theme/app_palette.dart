acc9ec35d99a43f574df21dd66455dfaea7dbf87
riaz27 <riaz.uddin27@gmail.com>
2025-09-18 13:43:35 +0600

    initial setup

---
+++ lib/core/theme/app_palette.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppPalette {
+  static const colorgg = Color(0xFFC13B20);
+  static const color1 = Color(0xFFFEF0D6);
+  static const color3 = Color(0xFF52180D);
+  static const color4 = Color(0xFFF0F0F0);
+  static const color3Variant = Color(0xFF591A0E);
+  static const color5 = Color(0xFFFF522F);
+}
---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:cardinal_quotes_task/core/theme/app_palette.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final lightThemeMode = ThemeData.light().copyWith(
+    scaffoldBackgroundColor: AppPalette.colorgg,
+  );
+}
